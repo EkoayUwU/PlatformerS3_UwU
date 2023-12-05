47abda194074ea5c8141873ea7f2782eec8e9566
Ekoay <ethangongora24@gmail.com>
2023-12-05 10:02:40 +0100

    RebuildModule

--- Source/PlatformerS3_UwU/PlatformerS3_UwU.Build.cs
+++ Source/PlatformerS3_UwU/PlatformerS3_UwU.Build.cs
@@ -10,7 +10,9 @@ public class PlatformerS3_UwU : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[] { "PlatformerS3_UwU" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
